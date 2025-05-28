73a3015b64de3b08d8d3185878ca70bb10a00d1d
erika <erika.morales@davinci.edu.ar>
2025-05-28 12:37:34 -0300

    init

---
+++ src/components/Template.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+<template>
+  <slot name="parrafo"></slot>
+  <div><slot></slot></div>
+</template>
+
+<style scoped>
+div{
+  color: red
+}
+</style>
---
+++ src/views/ListadoView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Titulo from '../components/Titulo.vue'
+import Template from '../components/Template.vue'
+</script>
+<template>
+  <Titulo texto="hola soy un titulo" />
+  <Template>
+    <template #parrafo>
+      <p>Esto se muestra solo en párrafo</p>
+    </template>
+    <p>Todo lo que esté dentro va a slot</p>
+  </Template>
+</template>
